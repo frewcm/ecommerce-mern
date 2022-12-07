139357b74f56e215e5e5b0cd82124014c12bb30e
frewcm <frewbefekadu32@gmail.com>
2022-12-07 12:04:35 +0300

    intro updated

--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-scroll";
 function Intro() {
   return (
     <div className="bg-center bg-cover bg-[url('../../public/images/Shopimage.jpg')] h-screen w-full">
-      <div className="h-screen w-full bg-green-300 bg-opacity-20 flex items-center justify-center">
+      <div className="h-screen w-full bg-green-700 bg-opacity-40 flex items-center justify-center">
         <div className="flex flex-col justify-center items-center text-gray-50">
-          <p className="text-2xl">Welcome.</p>
-          <p className="text-3xl lg:text-5xl mt-5 mb-5 text-black text-center">
+          <p className="text-2xl text-green-100">Welcome.</p>
+          <p className="text-3xl lg:text-5xl mt-5 mb-5 text-green-100 text-center">
             Stay Comfy, Cute and <br /> Sustainable.All Day. All Year.
           </p>
           <Link
@@ -16,7 +16,7 @@ function Intro() {
             smooth={true}
             offset={50}
             duration={500}
-            className=" w-28 p-2 border-2 border-green-900 bg-white text-green-900 duration-300 ease-in-out hover:bg-green-900 hover:text-white text-center rounded"
+            className=" w-28 p-2 border-2 border-green-900 bg-white text-green-900 duration-300 ease-in-out hover:bg-green-100 hover:text-black text-center rounded"
           >
             Shop
           </Link>
